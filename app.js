7a99337e2fece0fe6d8e7a68ca469a34273c682d
PranavTelvekar <160746050+PranavTelvekar@users.noreply.github.com>
2024-11-19 12:19:15 +0530

    Update app.js
    MongoDB Atlas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/SignUpLogin');
+  await mongoose.connect('mongodb+srv://Pranav:123@cluster0.sym7s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
